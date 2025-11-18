e19f5a8d384820699ad13e6ce7a9409d09bbcb92
Jeff Kim <98762871+20018040@users.noreply.github.com>
2025-11-17 22:20:58 -0800

    Time: 14 ms (28.99%) | Memory: 46.1 MB (15.16%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -3,7 +3,6 @@ class Solution {
         s = s.replaceAll("[^A-Za-z0-9]", "");
         s = s.toLowerCase();
         int size = s.length();
-        if(size ==1)return true;
         for(int i = 0; i<size/2;i++){
             if(s.charAt(i) != s.charAt(size-i-1)){
                 return false;
