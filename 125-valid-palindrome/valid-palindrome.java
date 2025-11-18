f5874d8af75b529cc1c6c638fa14129674918a6f
Jeff Kim <98762871+20018040@users.noreply.github.com>
2025-11-17 22:42:10 -0800

    Time: 2 ms (98.55%) | Memory: 44.1 MB (55.37%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,10 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // s = s.replaceAll("[^A-Za-z0-9]", "");
-        // s = s.toLowerCase();
-        int size = s.length();
         int left = 0;
-        int right = size -1;
+        int right = s.length() -1;
         while (left < right){
             char l = s.charAt(left);
             char r = s.charAt(right);
