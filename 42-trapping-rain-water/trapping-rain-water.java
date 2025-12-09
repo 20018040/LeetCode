329b8f83e67d57358ebef02c597df569ce6ddd74
Jeff Kim <98762871+20018040@users.noreply.github.com>
2025-12-08 19:28:18 -0800

    Time: 175 ms (5.69%) | Memory: 47.3 MB (57.04%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int highestPlace(int starting,int[] height){
+        int highest = -1;
+        int compare = 1;
+        while(starting < height.length){
+            if (height[starting] >= compare){
+                compare = height[starting];
+                highest = starting;
+            }
+            starting ++;
+        }
+        return highest;
+    }
+    public int trap(int[] height) {
+        int hLength = height.length;
+        if(hLength ==1 )
+            return 0;
+        int answer = 0;
+        int lowest = 0;
+        int i = 0;
+        // while(height[i] == 0){
+        //     i++;
+        // }
+        int leftPlace = i;
+        int heightL = height[leftPlace];
+        for(i= leftPlace +1; i< hLength;i++){
+            int heightI = height[i];
+            if(heightI >= heightL){
+                int x = i-1;
+                while(x>leftPlace){
+                    answer += (heightL-height[x]);
+                    x--;
+                }
+                leftPlace = i;
+                heightL = heightI;                
+            }
+        }
+        int tallestPlace = highestPlace(leftPlace+1,height);
+        while(tallestPlace != -1){
+            
+            int tHeight = height[tallestPlace];
+            int left = tallestPlace -1;
+            while(left > leftPlace){
+                answer += tHeight - height[left];
+                left--;
+            }
+            leftPlace = tallestPlace;
+            tallestPlace = highestPlace(tallestPlace+1,height);
+            
+        }
+        return answer;
+    }
+}
