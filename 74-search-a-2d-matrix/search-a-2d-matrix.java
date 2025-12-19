6e8224f3c05d2f80ba1681c8007008df819a7502
Jeff Kim <98762871+20018040@users.noreply.github.com>
2025-12-19 00:54:35 -0800

    Time: 0 ms (100.00%) | Memory: 44 MB (25.79%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -4,20 +4,22 @@ class Solution {
         int column = 0;
         int left = 0;
         int right = matrix.length-1;
-        while(left<= right){
-            row = (left+right)/2;
-            if(matrix[row][0] < target){
-                left = row +1; 
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (target < matrix[mid][0]) {
+                right = mid - 1;
+            } else if (target > matrix[mid][matrix[0].length - 1]) {
+                left = mid + 1;
+            } else {
+                row = mid;
+                break;
             }
-            else if(matrix[row][0] > target){
-                right = row -1;
-            }
-            else return true;
-            
         }
-        left = 0;
+        
         if(row != 0 && matrix[row][0] > target)
          row--;
+         left = 0;
         // if (row ==2) return true;
         right = matrix[row].length-1;
         while(left<= right){
