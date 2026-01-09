47c5b9d1a3027802bc4c3e88048e822014e3b228
Jeff Kim <98762871+20018040@users.noreply.github.com>
2026-01-08 23:35:00 -0800

    Time: 2 ms (99.72%) | Memory: 45.5 MB (81.98%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,46 +1,49 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int size = t.length();
-        String ans = "";
-        if(s.length() < size){
-            return "";
-        }
-        if(s.equals(t)){
-            return s;
-        }
-        
-        int[] nums = new int[58];
-        int left = 0;
-        int right = 1;
-        for(char c: t.toCharArray()){
-            nums[c-'A']++;
-        }
-        int[] com = new int [58];
+        if (s.length() < t.length()) return "";
 
-        int found = 0;
-        for(char c : s.toCharArray()){
-            int num = c -'A';
-            if(nums[num] > 0){
-                com[num]++;
-                if(com[num] <= nums[num])
-                    found++;
-                while(found == size){
-                    if(ans.isEmpty() || right-left < ans.length())
-                        ans = s.substring(left,right);
-                    int z = s.charAt(left) - 'A';
-                    com[z]--;
-                    if(com[z] < nums[z]) found--;
-                    left++;
-                    while(left < s.length() &&nums[s.charAt(left)-'A'] ==0 ){
-                       left++;
-                        
-                    }
+        int[] freq = new int[128]; // ASCII safe
+        int required = 0;
 
-                }
+        // Build frequency map for t
+        for (char c : t.toCharArray()) {
+            if (freq[c] == 0) required++; // count unique chars
+            freq[c]++;
+        }
+
+        int left = 0, right = 0;
+        int formed = required;
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
+
+        while (right < s.length()) {
+            char r = s.charAt(right);
+            freq[r]--;
+
+            if (freq[r] == 0) {
+                formed--; // one unique requirement satisfied
             }
-            else if(found == 0) left++;
+
+            // Try shrinking window
+            while (formed == 0) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                char l = s.charAt(left);
+                freq[l]++;
+
+                if (freq[l] > 0) {
+                    formed++; // requirement broken
+                }
+                left++;
+            }
             right++;
         }
-        return ans;
+
+        return minLen == Integer.MAX_VALUE
+            ? ""
+            : s.substring(start, start + minLen);
     }
 }
