1a179e155685a566a8690d18a90af10b0be913dc
Jeff Kim <98762871+20018040@users.noreply.github.com>
2026-01-28 01:23:16 -0800

    Time: 3 ms (65.69%) | Memory: 46.5 MB (62.33%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -15,8 +15,7 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root == null && subRoot == null) return true;
-        if(root == null || subRoot== null) return false;
+        if(root == null) return false;
         if(root.val == subRoot.val){
             if(isMatch(root,subRoot)) return true;
         }
